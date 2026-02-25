201166a77384879da6a4984eb558c8d88cb053ef
BigHardWin10 <win_tree@163.com>
2026-02-25 15:07:07 +0800

    我就不信还不行

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ async function handleRequests(request) {
     }else if(url.pathname == "/prescript"){
         return getPrescript()
     }else{
-        var resp = new Response({"status": "failed","data": "","message": "404 Not Found"})
-        resp.status = 404
+        var resp = new Response(JSON.stringify({"status": "failed","data": "","message": "404 Not Found"}))
         return resp
     }
 }
 
 async function checkService() {
-    return new Response({"status": "success","data": "","message": "服务正常运行"},{headers: { 'content-type': 'application/json' }})
+    return new Response(JSON.stringify({"status": "success","data": "","message": "服务正常运行"}),{headers: { 'content-type': 'application/json' }})
 }
 
 async function getPrescript() {
     const prescripts = await fetch("https://cdn.jsdelivr.net/gh/WIN-TREE/PrescriptAPI@latest/prescripts.json").json()
-    return new Response({"status": "success","data": prescripts[Math.floor(Math.random() * prescripts.length)],"message": "部分指令由Deepseek编写，剩余来自xiaomu1999f-cpu.github.io与nyos.dev"},{headers: { 'content-type': 'application/json' }})
+    return new Response(JSON.stringify({"status": "success","data": prescripts[Math.floor(Math.random() * prescripts.length)],"message": "部分指令由Deepseek编写，剩余来自xiaomu1999f-cpu.github.io与nyos.dev"}),{headers: { 'content-type': 'application/json' }})
 }
