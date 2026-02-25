5ddab38ef146e5fc083abb6658da42e59002f29d
BigHardWin10 <win_tree@163.com>
2026-02-25 15:25:12 +0800

    CORS

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const router = AutoRouter()
 router.get("/",checkService).get("/prescript",getPrescript)
 
 async function checkService() {
-    return new Response(JSON.stringify({"status": "success","data": "","message": "服务正常运行"}),{headers: { 'content-type': 'application/json' }})
+    return new Response(JSON.stringify({"status": "success","data": "","message": "服务正常运行"}),{headers: { 'Content-Type': 'application/json','Access-Control-Allow-Origin': '*' }})
 }
 
 async function getPrescript() {
     const resp = await fetch("https://cdn.jsdelivr.net/gh/WIN-TREE/PrescriptAPI@latest/prescripts.json")
     const prescripts = await resp.json()
-    return new Response(JSON.stringify({"status": "success","data": prescripts[Math.floor(Math.random() * prescripts.length)],"message": "部分指令由Deepseek编写，剩余来自xiaomu1999f-cpu.github.io与nyos.dev"}),{headers: { 'content-type': 'application/json' }})
+    return new Response(JSON.stringify({"status": "success","data": prescripts[Math.floor(Math.random() * prescripts.length)],"message": "部分指令由Deepseek编写，剩余来自xiaomu1999f-cpu.github.io与nyos.dev"}),{headers: { 'Content-Type': 'application/json','Access-Control-Allow-Origin': '*' }})
 }
 
 export default router
